40fe956edb7947c9e9acb48d424a3ad234af0cd5
tanzim721 <tanzimulislam799@gmail.com>
2023-04-30 10:03:55 +0600

    30-04-2023
    ok1

---
+++ site/public/js/custom.js
@@ -0,0 +1,14 @@
+// $("#Objectives").click(function(){
+//     var value = this.innerHTML;
+//     $(".btn11").text(value);
+// })
+// $(".btn12").click(function(){
+//     var value = this.innerHTML;
+//     $(".myButton1").text(value);
+// })
+// $(".btn13").click(function(){
+//     var value = this.innerHTML;
+//     $(".myButton1").text(value);
+// })
+
+
--- site/resources/views/Service/view.blade.php
+++ site/resources/views/Service/view.blade.php
@@ -2,20 +2,30 @@
     <h2 class="text-center p-3 text-bold">@yield('page_name')</h2>
     <div class="row ">
         @foreach($ServicesData as $key => $services)
-        <div class="col-md-3 width-100%; height-450px; p-2">
-            <div class="card" style="width: 16rem;" >
-                <img style="width: 100%; height:250px" class="card-img-top" src="{{$services->service_img}}" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">{{$services->service_name}}</h5>
-                    <p class="card-text">{{$services->service_des}}</p>
-                    <br>
-                    {{-- <time class="entry-data published" datetime=""><?php echo date("m/d/Y"); ?></time> --}}
-                    <a href="#" class="btn btn-primary">Go Details</a>
-                </div>      
-            </div>
-        </div> 
+            <div class="col-md-3 width-100%; height-450px; p-2">
+                <div class="card" style="width: 16rem;" >
+                    <img style="width: 100%; height:250px" class="card-img-top" src="{{$services->service_img}}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$services->service_name}}</h5>
+                        <p class="card-text">{{$services->service_des}}</p>
+                        <br>
+                        {{-- <time class="entry-data published" datetime=""><?php echo date("m/d/Y"); ?></time> --}}
+                        <a href="#" class="btn btn-primary">Go Details</a>
+                    </div>      
+                </div>
+            </div> 
         @endforeach
     </div>
+    {{-- <div class="row myButton1">
+
+    </div>
+    <div class="btn">
+        <button class="btn11">Button1</button>
+        <button class="btn12">Button2</button>
+        <button class="btn13">Button3</button>
+    </div> --}}
+
+
 </div>
 
   
