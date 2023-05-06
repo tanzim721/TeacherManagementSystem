db63f58f985e82b4918beadec8ff6c65809b7b80
tanzim721 <tanzimulislam799@gmail.com>
2023-05-06 21:10:28 +0600

    06-05-2023
    ok3,,,,,,,admin teachers modify for Course code and teacher controller.

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-    
+
 Route::GET('/', 'HomeController@HomeIndex');
 Route::GET('/visitor', 'VisitorController@VisitorIndex')->name('visitor');
 
