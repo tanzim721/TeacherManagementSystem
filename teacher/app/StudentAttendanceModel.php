1424a20cf477dfc4ece81edf34d2fdbf14b3041a
tanzim721 <tanzimulislam799@gmail.com>
2023-05-09 22:22:54 +0600

    h
    fh

--- teacher/app/StudentAttendanceModel.php
+++ teacher/app/StudentAttendanceModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudentAttendanceModel extends Model
 {
+     
     public $table='student_attendance_models';
     public $primaryKey='id';
     public $incrementing=true;
