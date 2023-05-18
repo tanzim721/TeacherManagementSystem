e02c184fe4b22a51ce2ac127c9e0fb82c4dc5e4d
tanzim721 <tanzimulislam799@gmail.com>
2023-05-18 23:48:12 +0600

    18-06-2023

--- teacher/app/LoginModel.php
+++ teacher/app/LoginModel.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ToDoModel extends Model
+class LoginModel extends Model
 {
     //
 }
--- teacher/database/migrations/2023_05_18_174011_create_login_models_table.php
+++ teacher/database/migrations/2023_05_18_174011_create_login_models_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateToDoModelsTable extends Migration
+class CreateLoginModelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateToDoModelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('to_do_models', function (Blueprint $table) {
+        Schema::create('login_models', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateToDoModelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('to_do_models');
+        Schema::dropIfExists('login_models');
     }
 }
